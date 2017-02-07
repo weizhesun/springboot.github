54158473be6d94fd3fd7b6b8819f669d6d6476cc
sunweizhe <weizhede@163.com>
2017-02-07 14:15:08 +0800

    addDemo

---
+++ src/main/java/com/jr/app/threadDemoTest/AddDemo.java
@@ -0,0 +1,10 @@
+package com.jr.app.threadDemoTest;
+
+/**
+ * Created by Administrator on 2017/2/7 0007.
+ */
+public class AddDemo {
+    public static void main(String[] args) {
+        System.out.println("add");
+    }
+}
